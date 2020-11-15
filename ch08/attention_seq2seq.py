5e36fd11c6ed0c3cf00009594c6c26fea35961fb
false <false@wizard-limit.net>
2020-11-15 20:21:57 +0900

    black ch08

--- ch08/attention_seq2seq.py
+++ ch08/attention_seq2seq.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 import sys
-sys.path.append('..')
-from common.time_layers import *
+
+sys.path.append("..")
+from common.time_layers import np, TimeEmbedding, TimeLSTM, TimeAffine, TimeSoftmaxWithLoss
 from ch07.seq2seq import Encoder, Seq2seq
 from ch08.attention_layer import TimeAttention
 
@@ -23,12 +24,12 @@ class AttentionDecoder:
         V, D, H = vocab_size, wordvec_size, hidden_size
         rn = np.random.randn
 
-        embed_W = (rn(V, D) / 100).astype('f')
-        lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
-        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
-        lstm_b = np.zeros(4 * H).astype('f')
-        affine_W = (rn(2*H, V) / np.sqrt(2*H)).astype('f')
-        affine_b = np.zeros(V).astype('f')
+        embed_W = (rn(V, D) / 100).astype("f")
+        lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype("f")
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype("f")
+        lstm_b = np.zeros(4 * H).astype("f")
+        affine_W = (rn(2 * H, V) / np.sqrt(2 * H)).astype("f")
+        affine_b = np.zeros(V).astype("f")
 
         self.embed = TimeEmbedding(embed_W)
         self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
@@ -42,7 +43,7 @@ class AttentionDecoder:
             self.grads += layer.grads
 
     def forward(self, xs, enc_hs):
-        h = enc_hs[:,-1]
+        h = enc_hs[:, -1]
         self.lstm.set_state(h)
 
         out = self.embed.forward(xs)
@@ -58,7 +59,7 @@ class AttentionDecoder:
         N, T, H2 = dout.shape
         H = H2 // 2
 
-        dc, ddec_hs0 = dout[:,:,:H], dout[:,:,H:]
+        dc, ddec_hs0 = dout[:, :, :H], dout[:, :, H:]
         denc_hs, ddec_hs1 = self.attention.backward(dc)
         ddec_hs = ddec_hs0 + ddec_hs1
         dout = self.lstm.backward(ddec_hs)
